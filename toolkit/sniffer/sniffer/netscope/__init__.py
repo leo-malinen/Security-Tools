d068f82862b6a42b9394cbdc4ece9a3e567b9480
Leo Malinen <107366630+leo-malinen@users.noreply.github.com>
2026-08-01 17:42:22 -0700

    Delete toolkit/sniffer directory

--- toolkit/sniffer/sniffer/netscope/__init__.py
+++
@@ -1,8 +0,0 @@
-from .decoder import decode
-from .detect import Alert, Detector, DetectorConfig
-from .filters import FilterError, compile_filter
-from .models import Packet
-from .pcap import PcapReader, PcapWriter
-from .stats import Statistics
-__version__ = '1.0.0'
-__all__ = ['Alert', 'Detector', 'DetectorConfig', 'FilterError', 'Packet', 'PcapReader', 'PcapWriter', 'Statistics', '__version__', 'compile_filter', 'decode']
